163be01221086064bf5fa0d20325325a07a5a594
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2022-05-30 21:47:59 +0200

    Update central_install.sh

--- central_install.sh
+++ central_install.sh
@@ -6,8 +6,6 @@ set -x
 
 cd "$(dirname "$0")/.."
 
-echo  "Authentication=VncAuth" | sudo tee -a  /etc/vnc/config.d/common.custom
-
 echo "=> Installing apache...\n"
 sudo apt update
 sudo apt install apache2 -y
@@ -24,19 +22,9 @@ echo "<?php phpinfo ();?>" > /var/www/html/index.php
 
 # start upp VNC
 echo " VNC"
+sudo apt install realvnc-vnc-server realvnc-vnc-viewer
 sudo vncpasswd -service
+echo  "Authentication=VncAuth" | sudo tee -a  /etc/vnc/config.d/common.custom
 # sudo systemctl start vncserver-x11-serviced.service
 sudo systemctl enable vncserver-x11-serviced.service
 sudo systemctl restart vncserver-x11-serviced
-
-# We have a script "listen-for-shutdown.py", which will shutdown the PI when triggered by a switch.  
-# We need to start this script on boot. So we'll place the script in /usr/local/bin and make it executable:
-sudo cp 'listen-for-shutdown.py' /usr/local/bin/
-sudo chmod +x '/usr/local/bin/listen-for-shutdown.py'
-
-# Now add another script called listen-for-shutdown.sh that will start/stop our service.
-# Place this file in /etc/init.d and make it executable.
-sudo cp 'listen-for-shutdown.sh' /etc/init.d/
-sudo chmod +x '/etc/init.d/listen-for-shutdown.sh'
-# Now we'll register the script to run on boot.
-sudo update-rc.d 'listen-for-shutdown.sh' defaults
